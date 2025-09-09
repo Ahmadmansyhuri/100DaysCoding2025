471efeb78a5fa4fe800951d0d6fe114fcc96a152
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-09-09 05:22:09 -0700

    Create Days002.java
    penggunaan println(), print(), dan printf().

---
+++ Days002.java
@@ -0,0 +1,20 @@
+
+package isc100dayscooding;
+
+public class Days002 {
+    public static void main(String[] args) {
+        //penguunaan println
+        System.out.println("ahmad");
+        System.out.println("manshyuri");
+        
+        //penggunaan print
+        System.out.print("ahmad");
+        System.out.print("mansyhuri");
+        
+        //penggunaan printf
+        System.out.printf("%n");
+        System.out.printf("%.2f%n",123.456);
+        System.out.printf("%.2e%n",170906.2006);
+    }
+    
+}
