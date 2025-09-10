c7571354e85376256a59cc3d84cbf04d82a660e6
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-09-10 05:35:12 -0700

    Create Days003.java
    Memahami dan menggunakan komentar

---
+++ Days003.java
@@ -0,0 +1,17 @@
+package isc100dayscooding;
+
+public class Days003 {
+    public static void main(String[] args) {
+        //penggunaan komentar satu baris
+        //System.out.println("Ahmad Manshyuri");
+        
+        /*penggunaan komentar
+        lebih dari satu baris */
+        
+        /* System.out.println("program studi");
+        System.out.println("informatika");
+        System.out.println("universitas sulawesi barat");
+        */
+    }
+    
+}
