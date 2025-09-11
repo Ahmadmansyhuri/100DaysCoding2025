bf7d5e3216c62a84c1ff3494e32e77dbd7ff4e23
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-09-11 20:09:08 +0800

    Create Days004.java
    penggunaan Escape Squence (\n, \t,\r, \',\",\\.)

---
+++ Days004.java
@@ -0,0 +1,25 @@
+package isc100dayscooding;
+
+public class Days004 {
+    public static void main(String[] args) {
+        // kode \n menambah baris baru (enter)
+        System.out.println("halo teman teman perkenalkan\nnama saya ahmad mansyhuri");
+        
+        // kode \t Menggerakkan kursor ke tab berikutnya (Tab)
+        System.out.println("saya mahasiswa baru di\tuniversitas\tsulawesi\tbarat");
+        
+        // kode \' Digunakan untuk mencetak karakter petik tunggal (‘) (single quote)
+        System.out.println("\'Informatika\'");
+        
+        //kode \" Digunakan untuk mencetak karakter petik ganda (") doouble quote))
+        System.out.println("\"BERSAMA ISC BERANTAS BUTA COODING\"");
+        
+        //kode \\ Digunakan untuk mencetak karakter backslash (\)
+        System.out.println("\t\\Dimana ada kemauan"); 
+        System.out.println("\t Disitu ada jalan\\");
+        
+        //kode \r mengembalikan kursor ke awal baris atau "menimpa" (carriage return)
+        System.out.println("nama =\rahmad mansyhuri \nasal =\rmamuju \ntanggal lahir =\r170906");
+    }
+    
+}
