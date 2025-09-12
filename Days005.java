59049a2b640254f071909a6a550cd9211ea13eb6
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-09-12 21:16:37 +0800

    Create Days005.java
    Tipe Data Numerik Bilangan Bulat (byte, short, int, long).

---
+++ Days005.java
@@ -0,0 +1,16 @@
+package isc100dayscooding;
+
+public class Days005 {
+    public static void main(String[] args) {
+        byte umur = 19;
+        short beratbadan = 59;
+        int tinggibadan = 170;
+        long kodeposmamuju = 91511;
+        
+        System.out.println("umur ="+umur);
+        System.out.println("berat badan ="+beratbadan);
+        System.out.println("tinggi badan ="+tinggibadan);
+        System.out.println("kode pos mamuju = "+kodeposmamuju);
+    }
+    
+}
