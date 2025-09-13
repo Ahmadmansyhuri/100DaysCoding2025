f62c436009fd073ae6eb9a60d43e152a71c34d5a
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-09-14 00:20:24 +0800

    Create Days007.java
    Tipe Data Karakter (char) dan Boolean (boolean).

---
+++ Days007.java
@@ -0,0 +1,22 @@
+package isc100dayscooding;
+
+public class Days007 {
+    public static void main(String[] args) {
+        char ipk = 'A'; //Mendeklarasikan variabel char bernama 'ipk' dengan nilai 'A'
+        char simbolfavorit = '$'; //Contoh Variabel char dengan simbol
+        char angkafavorit = '9'; //contoh variabel char (angka) disimpan sebagai angka favorit
+        
+        boolean sayamahasiswa =true; //true berarti aktif
+        boolean sayaanakfekon =false; //variabel boolean yang lain.false berarti tidak
+        
+        System.out.println("Nilai IPK \t\t\t:\t"+ipk);
+        System.out.println("Simbol Favorit saya\t\t:\t"+simbolfavorit);
+        System.out.println("Angka Favorit saya\t\t:\t"+angkafavorit);
+        
+        System.out.println("Saya Mahasiswa unsulbar\t:\t"+sayamahasiswa);
+        System.out.println("saya adalah anak fekon \t:\t"+sayaanakfekon);
+        
+        
+    }
+    
+}
