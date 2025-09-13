c07963214c230d2a2aa09f78b100ffd550378fe3
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-09-13 22:57:43 +0800

    Create Days006.java
    Tipe Data Pecahan (double, float).

---
+++ Days006.java
@@ -0,0 +1,14 @@
+package isc100dayscooding;
+
+public class Days006 {
+    public static void main(String[] args) {
+        float nilaiujian = 89.7f;
+        double nilairatarata = 96.6;
+        
+        System.out.println("NILAI RATA RATA IJAZAH & NILAI UJIAN");
+        System.out.println("Nama \t\t\t:Ahmad mansyhuri\nAsal sekolah \t\t:MAN 1 MAMUJU");
+        System.out.println("Hasil Nilai Ujian \t:"+nilaiujian);
+        System.out.println("Nilai Rata-Rata Ijazah :"+nilairatarata);
+    }
+    
+}
