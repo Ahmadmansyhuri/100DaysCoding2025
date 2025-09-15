630dd27e3b9de5ebb5a0feb9ba7570e030282770
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-09-16 00:29:01 +0800

    Create Days009.java
    Deklarasi, inisialisasi, dan update variabel.

---
+++ Days009.java
@@ -0,0 +1,30 @@
+package isc100dayscooding;
+
+public class Days009 {
+    public static void main(String[] args) {
+        // DEKLARASI VARIABEL
+        int umur;
+        double nilaiujian;
+        float ipk;
+        
+        // INISIALISASI VARIABEL
+        umur = 18;
+        nilaiujian = 85.5;
+        ipk = 3.9f;
+        
+        System.out.println("UMUR :" + umur);
+        System.out.println("NILAI UJIAN :" + nilaiujian);
+        System.out.println("NILAI IPK :" + ipk);
+        
+        // UPDATE VARIABEL
+        umur = 19;
+        nilaiujian = 96.9;
+        ipk = 3.9f;
+        
+        System.out.println("\nsetelah update");
+        System.out.println("UMUR :" +umur);
+        System.out.println("NILAI UJIAN :" +nilaiujian);
+        System.out.println("IPK :" +ipk);
+    }
+    
+}
