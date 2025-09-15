b7d0a37669d9e959a72cf0ba5705ffb705346a46
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-09-15 23:42:41 +0800

    Create Days008.java
    pengenalan tipe data String untuk teks.

---
+++ Days008.java
@@ -0,0 +1,15 @@
+package isc100dayscooding;
+
+public class Days008 {
+    public static void main(String[] args) {
+        String nama = "Ahmad mansyhuri";
+        String nim = new String("D0225524");
+        System.out.println(nama);
+        System.out.println(nama.length());
+        System.out.println(nama.toUpperCase());
+        System.out.println(nama.toLowerCase());
+        System.out.println(nim);
+      
+    }
+    
+}
