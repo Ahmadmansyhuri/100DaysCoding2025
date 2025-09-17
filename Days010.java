ab46c399395a0a199e01943d5c229b0097bb5650
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-09-17 22:37:57 +0800

    Create Days010.java
    constanta (final variabel)

---
+++ Days010.java
@@ -0,0 +1,19 @@
+package isc100dayscooding;
+
+public class Days010 {
+    public static void main(String[] args) {
+    /*Constanta (Final Variabel)
+      Constanta adalah variabel yang nilainya tetap dan tidak bisa diubah selama 
+      program berjalan.Di Java, kita membuat constanta dengan kata kunci final.
+    */
+       final double nilaiijazah =96.7;
+       final long banyaknya_mahasiswa = 5000;
+        System.out.println("NILAI IJAZAH = " + nilaiijazah);
+        System.out.println("JUMLAH MAHASISWA UNSULBAR = " + banyaknya_mahasiswa);
+        
+        /*nilaiijazah = 85.5; jika komentar ini dimatikan maka akan terjadi eror
+                              karena nilaiijazah sudah final.
+        */
+        }
+    
+}
