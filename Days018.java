ad795d6292a8ebb32db03c936f676b0ebd7e2cfd
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-09-25 22:17:22 +0800

    Create Days018.java
    konversi otomatis (Dari tipe data kecil ke besar).

---
+++ Days018.java
@@ -0,0 +1,22 @@
+package isc100dayscooding;
+
+public class Days018 {
+    public static void main(String[] args) {
+        byte D = 127;
+        long E = D*10;
+        
+        System.out.println("=====(byte D = 127; & long E = D;)=====");
+        System.out.println();
+        System.out.println("NILAI BYTE = " + D);
+        System.out.println("NILAI LONG = " + E);
+        
+        float  G = 45.1980f;
+        double H = G;
+        
+        System.out.println("\n=====(float  G = 45.1980f; & double H = G;)=====");
+        System.out.println();
+        System.out.println("NILAI FLOAT  \t= " + G);
+        System.out.println("NILAI DOUBLE \t= " + H);
+    }
+    
+}
