850249365638b1d055f978ffef50e9636bf32c24
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-09-30 00:18:58 +0800

    Create Days023.java
    latihan : program luas persegi panjang

---
+++ Days023.java
@@ -0,0 +1,28 @@
+package isc100dayscooding;
+import java.util.Scanner;
+
+public class Days023 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        
+        System.out.println("===MENGHITUNG LUAS PERSEGI PANJANG NYA BERAPA===");
+        System.out.println("\n=========================");
+        System.out.println("MASUUKAN PANJANG");
+        double P = in.nextDouble();
+        
+        
+        System.out.println("MASUKKAN LEBAR NYA BERAPA");
+        double L = in.nextDouble();
+        System.out.println("---------------------------");
+        
+        double l = P * L;
+        
+        System.out.println("\n=================================");
+        System.out.println("PANJANG  \t= " +P);
+        System.out.println("LUAS     \t= " + L);
+        System.out.println("luas dari persegi panjang adalah : " + l);
+        System.out.println("-----------------------------------");
+        in.close();
+    }
+}
+    
