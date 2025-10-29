00722fd3d8f1d6a0e6e0445e380ee2af89f24b79
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-10-29 22:01:22 +0800

    Update Days052.java
    perulangan while.

--- Days052.java
+++ Days052.java
@@ -1,22 +1,30 @@
 package isc100dayscooding;
+
 import java.util.Scanner;
 
 public class Days052 {
+
     public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);
+        Scanner in = new Scanner(System.in);
         System.out.print("Masukkan angka awal : ");
         int a = in.nextInt();
-        System.out.print("Masukkan angka akhir : ");        
+        System.out.print("Masukkan angka akhir : ");
         int b = in.nextInt();
-        while (a <= b){
-            if (a %2 == 0) {
-            System.out.println(a + " : Angka ini bilangan genap");    
-            } else {
-                System.out.println(a + " : angka ini bilangan ganjil");
+        System.out.println("==============================");
+
+        if (a > b) {
+            System.out.println("Angka awal harus lebih kecil dari angka akhir !!!");
+        } else {
+            while (a <= b) {
+                if (a % 2 == 0) {
+                    System.out.println(a + " : Angka ini bilangan genap");
+                } else {
+                    System.out.println(a + " : angka ini bilangan ganjil");
+                }
+                a++;
             }
-              a++;
         }
-           in.close();
+        in.close();
     }
-    
+
 }
