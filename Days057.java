4a1326e326dd2c7062843dccd38812eb4a6b99c0
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-11-03 21:18:57 +0800

    Create Days057.java
    nested loop.

---
+++ Days057.java
@@ -0,0 +1,22 @@
+package isc100dayscooding;
+
+public class Days057 {
+
+    public static void main(String[] args) {
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("   ");
+            }
+            //System.out.println();
+            for (int j = 5; j > i; j--) {
+                System.out.print(" * ");
+            }
+            //System.out.println("");
+            for (int j = 5; j >= i; j--) {
+                System.out.print(" * ");
+            }
+            System.out.println("");
+        }
+    }
+
+}
