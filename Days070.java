7324f26f584a808c4bbeb84c8ae7b2b51f1fd3e6
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-11-16 22:47:18 +0800

    Create Days070.java
    membuat pola persegi panjang.

---
+++ Days070.java
@@ -0,0 +1,22 @@
+package isc100dayscooding;
+
+import java.util.Scanner;
+
+public class Days070 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan panjang : ");
+        int panjang = input.nextInt();
+        
+        System.out.print("Masukkan lebar : ");
+        int lebar = input.nextInt();
+        
+        for (int i = 0; i < lebar; i++) {  
+            for (int j = 0; j < panjang; j++) {  
+                System.out.print("* ");
+            }
+            System.out.println();
+        } 
+        input.close();
+    }
+}
