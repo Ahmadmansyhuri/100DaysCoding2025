18d279b648657683ac062bb9a957fd1ff4ac98a2
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-11-28 22:32:14 +0800

    Create Days082.java
    Latihan: mengisi elemen Array dan menampilkan semua elemen array dengan perulangan.

---
+++ Days082.java
@@ -0,0 +1,12 @@
+package isc100dayscooding;
+
+public class Days082 {
+    public static void main(String[] args) {
+        int [] array = {10, 20, 30, 40, 50};
+        int a = array.length;
+        
+        for (int i = 0; i < a; i++) {
+            System.out.println("Elemen ke-" + (i + 1) + "=" + array[i]);
+        }
+    } 
+}
