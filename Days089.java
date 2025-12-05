daafea9ea50a6950054f2d1bdfe6f91d680ea265
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-12-05 22:52:00 +0800

    Create Days089.java
    method void.

---
+++ Days089.java
@@ -0,0 +1,12 @@
+package isc100dayscooding;
+
+public class Days089 {
+    public static void main(String[] args) {
+        adaadaaja.hello();
+    }
+    public class adaadaaja {
+        static void hello () {
+         System.out.println("Halo nama saya ahmad mansyhuri");
+        }
+    }
+}
