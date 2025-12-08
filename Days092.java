7992b78183686973f2d188e042836c1e88991830
Ahmadmansyhuri <ahmadmashyuri17@gmail.com>
2025-12-08 23:01:37 +0800

    Create Days092.java
    keyword return.

---
+++ Days092.java
@@ -0,0 +1,14 @@
+package isc100dayscooding;
+
+public class Days092 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        
+        if (a > b) {
+            System.out.println("a lebih besar");
+            return;
+        }
+        System.out.println("b lebih besar");
+    }
+}
